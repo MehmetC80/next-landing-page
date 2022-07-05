4e8d02304261d4d438664b65a03d23cb5fc715a4
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-07-05 20:17:47 +0200

    static-site redering and fetch Data from an json file

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Banner } from '../components/Banner';
 
 import { Header } from '../components/Header';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className=''>
       <Head>
@@ -12,8 +12,28 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-
       <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6 '>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          {/* pull some data from server - API endpoints*/}
+          {exploreData?.map((item) => {
+            return <h1 className='text-xl'>{item.location}</h1>;
+          })}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('http://links.papareact.com/pyp').then(
+    (result) => result.json()
+  );
+
+  return {
+    props: {
+      exploreData: exploreData,
+    },
+  };
 }
